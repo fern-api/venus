bc628cac96057338ed590b37f6dbfd23950e472a
Deep Singhvi <dsinghvi@umich.edu>
2022-09-22 20:59:34 -0700

    run pre-commit

--- venus/generated/server/venus-api/src/__init__.py
+++ venus/generated/server/venus-api/src/__init__.py
@@ -1,13 +1,8 @@
 from .commons import OrganizationId, UserId
 from .organization import CreateOrganizationRequest, UserReference
-from .registry import (
-    CheckRegistryPermissionRequest,
-    GenerateRegistryTokensRequest,
-    MavenRegistryToken,
-    NpmRegistryToken,
-    RegistryToken,
-    RegistryTokens,
-)
+from .registry import (CheckRegistryPermissionRequest,
+                       GenerateRegistryTokensRequest, MavenRegistryToken,
+                       NpmRegistryToken, RegistryToken, RegistryTokens)
 from .user import OrganizationsPage, User
 
 __all__ = [
