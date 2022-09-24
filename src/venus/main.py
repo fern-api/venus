dc3c3b13353904a2c5c0a04d54bb846183cc6390
Deep Singhvi <deep@usebirch.com>
2022-09-23 17:19:21 -0700

    improvement: dummy endpoints implemented (#3)

--- src/venus/main.py
+++ src/venus/main.py
@@ -1,7 +1,14 @@
 import uvicorn
+
 from fastapi import FastAPI
 
+from venus import organization_service
+from venus import registry_service
+
+
 app = FastAPI()
+app.include_router(organization_service.router)
+app.include_router(registry_service.router)
 
 
 @app.get("/health")
---
+++ src/venus/organization_service.py
@@ -0,0 +1,16 @@
+from fastapi_utils.cbv import cbv
+from fastapi_utils.inferring_router import InferringRouter
+
+import venus.generated.server.venus_api.src.organization as fern
+
+
+router = InferringRouter()
+
+
+@cbv(router)
+class OrganizationsService:
+    @router.post("/organizations/create")
+    def create_organization(
+        self, request: fern.CreateOrganizationRequest
+    ) -> None:
+        print("Creating organization")
