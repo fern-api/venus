d690cc10c4c64538b4a609d42a028d757ad0b7e7
dsinghvi <deep@usebirch.com>
2022-09-26 09:02:54 -0700

    fix ci

--- src/venus/organization_service.py
+++ src/venus/organization_service.py
@@ -1,10 +1,11 @@
-from fastapi_utils.cbv import cbv
 from fastapi import APIRouter
 from fastapi import Depends
+from fastapi_utils.cbv import cbv
 
 import venus.generated.server.venus_api.src.organization as fern
-from venus.global_dependencies import get_auth0
+
 from venus.auth.auth0_client import Auth0Client
+from venus.global_dependencies import get_auth0
 
 
 router = APIRouter()
@@ -14,7 +15,9 @@ router = APIRouter()
 class OrganizationsService:
     @router.post("/organizations/create")
     def create_organization(
-        self, request: fern.CreateOrganizationRequest, auth0_client: Auth0Client = Depends(get_auth0)
+        self,
+        request: fern.CreateOrganizationRequest,
+        auth0_client: Auth0Client = Depends(get_auth0),
     ) -> None:
         print("Creating organization in Auth0")
         auth0_client.get().create_organization(org_id=request.organization_id)
--- tests/test_auth0_client.py
+++ tests/test_auth0_client.py
@@ -1,18 +1,23 @@
 from uuid import uuid4
+
+import pytest
+
 from venus.auth.auth0_client import Auth0Client
 from venus.config import VenusConfig
-import pytest
+
 
 @pytest.mark.skip(reason="requires hitting auth0")
 def test_auth0_client() -> None:
-    auth0_client = Auth0Client(config=VenusConfig(
-        auth0_client_id="8lyAgexpGrHZLhN2i1FNPSicjupACR1r",
-        auth0_client_secret="FILL_ME_IN",
-        auth0_domain_name="fern-dev.us.auth0.com",
-        auth0_mgmt_audience="https://fern-dev.us.auth0.com/api/v2/",
-        cloudmap_name="fake"
-    ))
+    auth0_client = Auth0Client(
+        config=VenusConfig(
+            auth0_client_id="8lyAgexpGrHZLhN2i1FNPSicjupACR1r",
+            auth0_client_secret="FILL_ME_IN",
+            auth0_domain_name="fern-dev.us.auth0.com",
+            auth0_mgmt_audience="https://fern-dev.us.auth0.com/api/v2/",
+            cloudmap_name="fake",
+        )
+    )
     venus_auth0_client = auth0_client.get()
-    generated_org_name = "test_" + str(uuid4());
+    generated_org_name = "test_" + str(uuid4())
     venus_auth0_client.create_organization(generated_org_name)
     print("Created org " + generated_org_name)
