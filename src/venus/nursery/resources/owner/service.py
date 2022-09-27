6c451379958b89078fc8e408b5531f6b3652668d
dsinghvi <deep@usebirch.com>
2022-09-26 17:57:54 -0700

    urls must contain origin in requests

--- src/venus/nursery/resources/owner/service.py
+++ src/venus/nursery/resources/owner/service.py
@@ -8,20 +8,24 @@ from .types import Owner
 from .types import OwnerId
 from .types import UpdateOwnerRequest
 
+from urllib.parse import urljoin
+
 
 class OwnerService:
     def __init__(self, origin: str):
-        self.origin = origin
+        self.origin = origin if origin.endswith("/") else origin + "/"
 
     def create(self, *, body: CreateOwnerRequest) -> Response[None, None]:
-        response = requests.post(url="/owner", data=body.json(by_alias=True))
+        response = requests.post(
+            url=urljoin(self.origin, "owner"), data=body.json(by_alias=True)
+        )
         if response.status_code >= 200 or response.status_code < 300:
             return SuccessResponse(ok=True, body=None)
         else:
             return FailedResponse(ok=False, error=None)
 
     def get(self, *, owner_id: OwnerId) -> Response[Owner, None]:
-        response = requests.get(url=f"/owner/{owner_id}")
+        response = requests.get(url=urljoin(self.origin, f"/owner/{owner_id}"))
         if response.status_code >= 200 or response.status_code < 300:
             return SuccessResponse(
                 ok=True, body=Owner.parse_raw(response.text)
@@ -33,7 +37,8 @@ class OwnerService:
         self, *, owner_id: OwnerId, body: UpdateOwnerRequest
     ) -> Response[Owner, None]:
         response = requests.put(
-            url=f"/owner/{owner_id}", data=body.json(by_alias=True)
+            url=urljoin(self.origin, f"/owner/{owner_id}"),
+            data=body.json(by_alias=True),
         )
         if response.status_code >= 200 or response.status_code < 300:
             return SuccessResponse(
