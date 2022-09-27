11f50ae339edab34fabf7cae9948a5682470eed0
dsinghvi <deep@usebirch.com>
2022-09-27 16:41:19 -0700

    export UpdateOrganizationRequest

--- src/venus/generated/server/venus_api/src/organization/__init__.py
+++ src/venus/generated/server/venus_api/src/organization/__init__.py
@@ -1,5 +1,10 @@
 from .create_organization_request import CreateOrganizationRequest
 from .user_reference import UserReference
+from .update_organization_request import UpdateOrganizationRequest
 
 
-__all__ = ["CreateOrganizationRequest", "UserReference"]
+__all__ = [
+    "CreateOrganizationRequest",
+    "UserReference",
+    "UpdateOrganizationRequest",
+]
