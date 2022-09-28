57815f87a4f9313337b0f0e0bb1789faf11b4fac
dsinghvi <deep@usebirch.com>
2022-09-27 17:01:53 -0700

    manually fix line length

--- tests/http_utils.py
+++ tests/http_utils.py
@@ -2,5 +2,5 @@ def assert_valid_status_code(status_code: int, endpoint: str) -> None:
     if status_code >= 200 and status_code < 300:
         return None
     raise Exception(
-        f"Received error for endpoint={endpoint} with status_code={status_code}"
+        f"Received status_code={status_code} for endpoint={endpoint}"
     )
